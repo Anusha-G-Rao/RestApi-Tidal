c53b9a96695d25b0711caac6c1cf62ea626ddffd
Anusha-G-Rao <anusha2701@gmail.com>
2022-11-13 12:22:27 +0100

    terraform script to pull image from dockerhub updated

--- docker_tf/main.tf
+++ docker_tf/main.tf
@@ -1,14 +1,13 @@
 # Start a container
+
+resource "docker_image" "restapi-tidal" {
+  name = "grao2701/restapi-tidal:latest"
+}
 resource "docker_container" "restapi-tidal" {
-  name  = "grao2701/restapi-tidal"
+  name = "restapi-tidal"
   image = docker_image.restapi-tidal.latest
   ports {
-    internal = "8080"
-    external = "8080"
+    internal = 8080
+    external = 8080
   }
-}
-
-# Find the latest Ubuntu precise image.
-resource "docker_image" "grao2701" {
-  name = "restapi-tidal"
 }
